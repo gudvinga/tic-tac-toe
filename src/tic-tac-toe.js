74504318562b3298b92cf409e8787fdec61659ef
gudvinga <gudvinga@gmail.com>
2017-09-24 23:30:53 +0300

    Work Done

--- src/tic-tac-toe.js
+++ src/tic-tac-toe.js
@@ -29,7 +29,15 @@ class TicTacToe {
     }
 
     getWinner() {
-        // return true;
+        var arr = this.matrix;
+        if ((arr[0][0] == arr[1][1] && arr[1][1] == arr[2][2]) || (arr[2][0] == arr[1][1] && arr[1][1] == arr[0][2])) return arr[1][1];
+        if (arr[0][0] == arr[0][1] && arr[0][1] == arr[0][2]) return arr[0][0];
+        if (arr[1][0] == arr[1][1] && arr[1][1] == arr[1][2]) return arr[1][0];
+        if (arr[2][0] == arr[2][1] && arr[2][1] == arr[2][2]) return arr[2][0];
+        if (arr[0][0] == arr[1][0] && arr[1][0] == arr[2][0]) return arr[0][0];
+        if (arr[0][1] == arr[1][1] && arr[1][1] == arr[2][1]) return arr[0][1];
+        if (arr[0][2] == arr[1][2] && arr[1][2] == arr[2][2]) return arr[0][2];
+        return null;
     }
 
     noMoreTurns() {
